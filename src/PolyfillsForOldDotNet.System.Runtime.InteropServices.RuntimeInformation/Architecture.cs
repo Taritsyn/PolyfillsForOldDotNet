454789cd15409e18849dd974c68210ea3f050a98
Andrey Taritsyn <taritsyn@gmail.com>
2018-11-27 14:00:06 +0300

    Added a polyfills for the `OSArchitecture` property and `IsOSPlatform` method of `RuntimeInformation` class from the System.Runtime.InteropServices.RuntimeInformation library

---
+++ src/PolyfillsForOldDotNet.System.Runtime.InteropServices.RuntimeInformation/Architecture.cs
@@ -0,0 +1,10 @@
+﻿namespace PolyfillsForOldDotNet.System.Runtime.InteropServices
+{
+	public enum Architecture
+	{
+		X86,
+		X64,
+		Arm,
+		Arm64
+	}
+}
---
+++ src/PolyfillsForOldDotNet.System.Runtime.InteropServices.RuntimeInformation/Utilities/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+using PolyfillsForOldDotNet.System.Runtime.InteropServices.Resources;
+
+namespace PolyfillsForOldDotNet.System.Runtime.InteropServices.Utilities
+{
+	internal static class Utils
+	{
+		internal static string ReadProcessOutput(string fileName)
+		{
+			return ReadProcessOutput(fileName, string.Empty);
+		}
+
+		internal static string ReadProcessOutput(string fileName, string args)
+		{
+			if (string.IsNullOrWhiteSpace(fileName))
+			{
+				throw new ArgumentException(Strings.Argument_EmptyValue, nameof(fileName));
+			}
+
+			string output;
+			var processInfo = new ProcessStartInfo
+			{
+				FileName = fileName,
+				Arguments = args ?? string.Empty,
+				UseShellExecute = false,
+				RedirectStandardOutput = true
+			};
+
+			using (Process process = Process.Start(processInfo))
+			{
+				output = process.StandardOutput.ReadToEnd();
+				process.WaitForExit();
+			}
+
+			return output;
+		}
+	}
+}
